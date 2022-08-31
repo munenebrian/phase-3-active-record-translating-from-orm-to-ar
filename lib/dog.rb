1760df1ec149471c05491a2607f006f377b13c10
munenebrian <njerubrianmunene@gmail.com>
2022-08-31 10:28:58 +0300

    commit tree 033e03b1f62b2490b38fc8e89de3b9cfbabfba60

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,23 @@
-class Dog
+require "pry"
+require "active_record"
+
+ActiveRecord::Base.establish_connection(
+  adapter: "sqlite3",
+  database: "db/dogs.sqlite"
+)
+
+sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS dogs(
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    breed TEXT
+  )
+SQL
+ActiveRecord::Base.connection.execute(sql)
+
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+
+class Dog < ActiveRecord::Base
+  
 end
